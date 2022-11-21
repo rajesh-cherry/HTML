1f78dacfa37d5df96a26fb205683da1f70ba3718
rajesh-cherry <pabbojurajesh927@gmail.com>
2022-11-21 11:00:39 +0530

    this is my html repo

---
+++ JAVA-SCRIPT/inbox.js
@@ -0,0 +1,30 @@
+var inbox = [
+    {
+        from:"",
+        reply-to: "",
+        to: "",
+        date: "",
+        subject: "",
+        mailed by: "",
+        signed by: "",
+        security: ""
+
+    }
+]
+
+console.log("Welcome to Programiz!");
+
+var inbox = [
+    {
+        from:"pabbojurajesh927@gmail.com",
+        replyTo:"Freelancer.com",
+        to: "pabbojurajesh28@gmail.com",
+        date: 'Sep 28, 2022, 7:09 AM',
+        subject: "Rajesh, these Linux, PHP, and Asterisk PBX projects might interest you",
+        security: "TLS"
+
+    }
+    
+    ]
+    
+    console.log (inbox);
